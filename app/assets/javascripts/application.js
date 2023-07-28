e8bc6797ab9a4117c555e1cce21af03d653441c7
Emma <ejames@companieshouse.gov.uk>
2023-07-28 12:53:41 +0100

    Updating the GPG45 flow - dynamic checkbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,59 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
-window.GOVUKPrototypeKit.documentReady(() => {
+
+//group 1
+var selectedDocuments = 0;
+
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
   // Add JavaScript here
+
+  $("input[name='group-1']").on('change', function () {
+  if ($(this).val() === "none") {
+    selectedDocuments=0;
+   // alert("none")
+  }
+  else if ($(this).prop('checked') === false) {
+    selectedDocuments--;
+//    alert(selectedDocuments);
+  } else {
+    selectedDocuments++;
+ //   alert(selectedDocuments);
+  }
+    var input = document.getElementById('hiddenInput');
+    input.value = selectedDocuments.toString();
 })
+})
+
+
+
+//group 2
+
+var group2Docs = 0;
+
+
+$(document).ready(function () {
+  window.GOVUKFrontend.initAll()
+  // Add JavaScript here
+
+  $("input[name='group-2']").on('change', function () {
+  if ($(this).val() === "none") {
+    group2Doc=0;
+   // alert("none")
+  }
+  else if ($(this).prop('checked') === false) {
+    group2Doc--;
+//    alert(group2Docs);
+  } else {
+    group2Doc++;
+ //   alert(group2Docs);
+  }
+    var input = document.getElementById('hiddenInput');
+    input.value = group2Docs.toString();
+})
+})
+
+
+
--- app/routes/routes-v4.js
+++ app/routes/routes-v4.js
@@ -11,15 +11,9 @@ const router = govukPrototypeKit.requests.setupRouter()
  * UK national 
  */
 router.post('/v4/documents-checked-1', function (req, res) {
-    
-
-    //if 3 documents are selected ->  res.redirect('documents-checked-6')
-
-    //if 2 documents are selected -> 
 
     res.redirect('documents-checked-2')
-   
-
+    
 }) 
 
 /*
@@ -27,8 +21,16 @@ router.post('/v4/documents-checked-1', function (req, res) {
  */
 router.post('/v4/documents-checked-2', function (req, res) {
     
+    //if 3 documents are selected ->  res.redirect('documents-checked-6')
 
-    res.redirect('documents-checked-3')
+    //if 2 documents are selected -> 
+    if (req.session.data['selectedDocs'] >= "3") {
+        res.redirect('documents-checked-6')
+    }
+    else if(req.session.data['selectedDocs'] <= "2") {
+
+        res.redirect('documents-checked-3')
+    }
 
 }) 
 
@@ -36,9 +38,16 @@ router.post('/v4/documents-checked-2', function (req, res) {
  * Group 3
  */
 router.post('/v4/documents-checked-3', function (req, res) {
-    
 
-    res.redirect('documents-checked-4')
+
+     //if 2 documents are selected -> 
+     if (req.session.data['group2Docs'] >= "2") {
+        res.redirect('documents-checked-6')
+    }
+    else if(req.session.data['group2Docs'] <= "2") {
+
+        res.redirect('documents-checked-4')
+    }
 
 }) 
 
@@ -47,11 +56,44 @@ router.post('/v4/documents-checked-3', function (req, res) {
  * address
  */
 router.post('/v4/documents-checked-6', function (req, res) {
+
+
+     //If it is the relevant officer, ask to confirm the statement. 
+     if (req.session.data['docs-address'] === 'yes') {
+        
+        res.redirect('documents-checked-7')
+    }
+    // Otherwise take them to a stop screen
+    else{
+
+        res.redirect('documents-checked-10')
+    }
     
 
-    res.redirect('documents-checked-7')
+}) 
+
+
+/*
+ * address unsual
+ */
+router.post('/v4/documents-checked-7', function (req, res) {
+
+
+     //If it is the relevant officer, ask to confirm the statement. 
+     if (req.session.data['address-issues'] === 'yes') {
+        res.redirect('documents-checked-8')
+        
+    }
+    // Otherwise take them to a stop screen
+    else{
+
+        res.redirect('documents-checked-9')
+
+
+    }
 
 }) 
+
 
 
 
