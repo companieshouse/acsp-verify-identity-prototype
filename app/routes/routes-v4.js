b035f1019c1168a2cfa1fa1dcc5b438cbe7621fe
Emma <ejames@companieshouse.gov.uk>
2023-08-03 23:07:59 +0100

    Changed routing to match policy
    Note all questions are now checked at the end, rather than having early drop outs. This enables group 3 to occur.

--- app/routes/routes-v4.js
+++ app/routes/routes-v4.js
@@ -33,13 +33,13 @@ router.post('/v4/documents-checked-2', function (req, res) {
     
     //if 3 documents are selected ->  res.redirect('documents-checked-6')
 
-    if (req.session.data['selectedDocs'] >= "3") {
-        res.redirect('documents-checked-6')
-    }
-    else {
+    // if (req.session.data['selectedDocs'] >= "3") {
+    //     res.redirect('documents-checked-6')
+    // }
+    // else {
 
         res.redirect('documents-checked-3')
-    }
+    // }
 
 }) 
 
@@ -53,38 +53,44 @@ router.post('/v4/documents-checked-3', function (req, res) {
     //1 document from Group 1 AND 2 or more documents are selected from Group 2 - END journey 
      /* Route 1
     */
-     if ((req.session.data['selectedDocs'] == "1") && (req.session.data['group2Docs'] >= "2")) {
-        
-        res.redirect('documents-checked-6')
 
-    }
-    /* Route 1
-    */
-    else if ((req.session.data['selectedDocs'] == "2") && (req.session.data['group2Docs'] >= "1")) {
-        
-        res.redirect('documents-checked-6')
+    // if (req.session.data['selectedDocs'] >= "3") {
+    //     res.redirect('documents-checked-6')
+    // }
 
-    }
+    // else if ((req.session.data['selectedDocs'] == "1") && (req.session.data['group2Docs'] >= "2")) {
+        
+    //     res.redirect('documents-checked-6')
+
+    // }
+    // /* Route 1
+    // */
+    // else if ((req.session.data['selectedDocs'] == "2") && (req.session.data['group2Docs'] >= "1")) {
+        
+    //     res.redirect('documents-checked-6')
+
+    // }
    
     
 
-    /* Route 2
-    */
-    else if ((req.session.data['selectedDocs'] == "0") && (req.session.data['group2Docs'] >= "3")) {
+    // /* Route 2
+    // */
+    // else if ((req.session.data['selectedDocs'] == "0") && (req.session.data['group2Docs'] >= "3")) {
 
-        res.redirect('documents-checked-12')
-        //different page after this - route 2 
-    }
+    //     res.redirect('documents-checked-12')
+    //     //different page after this - route 2 
+    // }
     
 
-    /* Route 1 and 2 (fall through to group 3)
-    */
+    // /* Route 1 and 2 (fall through to group 3)
+    // */
 
-    else{
+    // else{
 
-        res.redirect('documents-checked-4')
+    //     res.redirect('documents-checked-4')
 
-    }
+    // }
+    res.redirect('documents-checked-4')
 
 }) 
 
@@ -93,53 +99,48 @@ router.post('/v4/documents-checked-3', function (req, res) {
  */
 router.post('/v4/documents-checked-4', function (req, res) {
 
+
     /*
     * Route 1
     */
-    if((req.session.data['selectedDocs'] == "1") && (req.session.data['group3Docs'] >= "2")) {
-        
-        res.redirect('documents-checked-6')
+    if (req.session.data['selectedDocs'] >= "3") {
+         res.redirect('documents-checked-6')
     }
-    /* Route 1
-    */
-    else if ((req.session.data['selectedDocs'] == "2") && (req.session.data['group3Docs'] >= "1")) {
-        
-        res.redirect('documents-checked-6')
+    else if ((req.session.data['selectedDocs'] == "1") && (req.session.data['group2Docs'] >= "2")) {
+         res.redirect('documents-checked-6')
 
     }
-    /* Route 1
-    */
+    else if ((req.session.data['selectedDocs'] == "2") && (req.session.data['group2Docs'] >= "1")) 
+    {   
+         res.redirect('documents-checked-6')
+    }
+    else if((req.session.data['selectedDocs'] == "1") && (req.session.data['group3Docs'] >= "2")) {
+        
+        res.redirect('documents-checked-6')
+    }
+    else if ((req.session.data['selectedDocs'] == "2") && (req.session.data['group3Docs'] >= "1")) {   
+        res.redirect('documents-checked-6')
+    }
     else if((req.session.data['selectedDocs'] == "1") && (req.session.data['group2Docs'] == "1") && (req.session.data['group3Docs'] >= "1")){
         res.redirect('documents-checked-6')
-
-    }
-    /* Route 1
-    */
-    else if(req.session.data['selectedDocs'] >= "1"){
-        res.redirect('documents-checked-11')
     }
     
     /*
     * Route 2
     */
+    else if (req.session.data['group2Docs'] >= "3") {
+        res.redirect('documents-checked-12')
+    }
     else if ((req.session.data['group2Docs'] == "1") && (req.session.data['group3Docs'] >= "2")) {
-
-        res.redirect('documents-checked-12')
-        //different page after this - route 2 
+        res.redirect('documents-checked-12') 
     }
-     /* Route 2
-    */
     else if ((req.session.data['group2Docs'] == "2") && (req.session.data['group3Docs'] >= "1")) {
-
         res.redirect('documents-checked-12')
-        //different page after this - route 2 
     }
-    /* Route 2
-    */
-        else {
-            res.redirect('documents-checked-11')
-        }
-   //else?
+    //check?
+    else {
+        res.redirect('documents-checked-11')
+    }
 }) 
 
 
@@ -157,19 +158,19 @@ router.post('/v4/documents-checked-11', function (req, res) {
     else{
             if ((req.session.data['group2Docs'] >= "4") ) {
             
-                res.redirect('documents-checked-6')
+                res.redirect('documents-checked-7')
             }
             else if ((req.session.data['group2Docs'] == "1")  && (req.session.data['group3Docs'] >= "3")) {
                 
-                    res.redirect('documents-checked-6')
+                    res.redirect('documents-checked-7')
             }
             else if ((req.session.data['group2Docs'] == "2")  && (req.session.data['group3Docs'] >= "2")) {
                 
-                res.redirect('documents-checked-6')
+                res.redirect('documents-checked-7')
             }
             else if ((req.session.data['group2Docs'] == "3")  && (req.session.data['group3Docs'] >= "1")) {
                 
-                res.redirect('documents-checked-6')
+                res.redirect('documents-checked-7')
             }   
             else{
                 res.redirect('OUTkjskfjskljhi')
@@ -187,19 +188,34 @@ router.post('/v4/documents-checked-6', function (req, res) {
 
 
      //If it is the relevant officer, ask to confirm the statement. 
-     if (req.session.data['docs-address'] === 'yes') {
+    if (req.session.data['docs-address'] === 'yes') {
         
         res.redirect('documents-checked-7')
     }
     // Otherwise take them to a stop screen
     else{
-
-        res.redirect('documents-checked-10')
+        res.redirect('documents-checked-11')
     }
     
 
 }) 
 
+
+router.post('/v4/documents-checked-12', function (req, res) {
+
+
+    //If it is the relevant officer, ask to confirm the statement. 
+   if (req.session.data['address-DOB'] === 'yes') {
+       
+       res.redirect('documents-checked-7')
+   }
+   // Otherwise take them to a stop screen
+   else{
+       res.redirect('documents-checked-11')
+   }
+   
+
+}) 
 
 
 
